96a8248f87caac322014956618b44ce6cec06a21
Giovanni Almeida <giovanni.as.br@gmail.com>
2024-09-23 09:08:46 -0300

    feat: add status component to history page

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,4 +1,4 @@
-import { HistoryContainer, HistoryList } from './styles'
+import { HistoryContainer, HistoryList, Status } from './styles'
 
 export function History() {
   return (
@@ -19,20 +19,32 @@ export function History() {
             <tr>
               <td>Something</td>
               <td>25 minutes</td>
-              <td>3 months ago</td>
-              <td>Completed</td>
+              <td>1 month ago</td>
+              <td>
+                <Status statusColor='green'>
+                  Completed
+                </Status>
+              </td>
             </tr>
             <tr>
               <td>Something</td>
               <td>20 minutes</td>
               <td>3 months ago</td>
-              <td>Completed</td>
+              <td>
+                <Status statusColor='yellow'>
+                  In progress
+                </Status>
+              </td>
             </tr>
             <tr>
               <td>Something</td>
               <td>40 minutes</td>
-              <td>3 months ago</td>
-              <td>Completed</td>
+              <td>6 months ago</td>
+              <td>
+                <Status statusColor='red'>
+                  Interrupted
+                </Status>
+              </td>
             </tr>
           </tbody>
         </table>
